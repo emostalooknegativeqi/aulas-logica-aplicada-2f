ad31d65feb7de16f1362ca040f09c369a4433c74
emostalooknegativeqi <emostalook@gmail.com>
2023-05-18 08:25:06 -0300

    omgtheykilledkenny

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hayloft II (SMASHUP)");
+        // aula de orientação de objetos
+        //declarar objetos
+        Pessoa stan;
+        //instanciar objeto
+        stan = new Pessoa();
+        Pessoa eric = new Pessoa();
+        //definição do formato do objeto
+        stan.nome = "Stan";
+        stan.sobrenome = "Marsh";
+        eric.nome = "Eric";
+        eric.sobrenome = "Cartman";
+        //acionar comportamento
+        stan.falar();
+        eric.falar();
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+    //atributos da classe
+    String nome;
+    String sobrenome;
+
+    //metodo da classe
+    public void falar(){
+        System.out.println("omg they killed kenny");
+    }
+}
