3f7522665d686ac78e11c90cf6c29776eb897ae1
emostalooknegativeqi <emostalook@gmail.com>
2023-05-18 07:19:37 -0300

    matagalkk

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("deixei hmmm 30 reais hmm debaixo do vaso no corredor do lado do hmm banheiri");
     }
 }
