1f0686d6070d614b01d60f7ee137e74e8b376b9f
emostalooknegativeqi <emostalook@gmail.com>
2023-05-18 07:27:35 -0300

    vasco kk

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("deixei hmmm 30 reais hmm debaixo do vaso no corredor do lado do hmm banheiri");
+        System.out.println("kkkkkkkkkkk");
     }
 }
