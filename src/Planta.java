3e736c0732f9010e23a5a376bf48ee3beab3fec9
Caua Santanna <emostalook@gmail.com>
2023-06-13 08:01:48 -0300

    to brincando de ser deus kk

---
+++ src/Planta.java
@@ -0,0 +1,4 @@
+public class Planta {
+    public Planta(Semente s1) {
+    }
+}
--- src/Solo.java
+++ src/Solo.java
@@ -1,2 +1,14 @@
 public class Solo {
+
+    //atributos
+    String tipo;
+    String cor;
+    String substancia;
+    boolean fertil;
+
+    //metodos
+    public Planta gerarPlanta(Semente s1){
+        Planta p1 = new Planta(s1);
+        return p1;
+    }
 }
